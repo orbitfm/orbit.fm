cddf35711cc7a95a8de978215b5c3a2dd4e191ff
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-01-08 20:35:10 -0700

    Fix build

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -8,11 +8,15 @@ import React from 'react';
     <div>{data.contentfulEpisode.shortDescription}</div>
     <div>{data.contentfulEpisode.publicationDate}</div>
     <div>{data.contentfulEpisode.audioUrl}</div>
-    <div>{data.contentfulEpisode.hosts.map(host =>
-      <div key={host.id}>{host.name}</div>)}
+    <div>{data.contentfulEpisode.hosts &&
+        data.contentfulEpisode.hosts.map(host =>
+          <div key={host.id}>{host.name}</div>
+        )}
     </div>
-    <div>{data.contentfulEpisode.guests.map(guest =>
-      <div key={guest.id}>{guest.name}</div>)}
+    <div>{data.contentfulEpisode.guests &&
+        data.contentfulEpisode.guests.map(guest =>
+          <div key={guest.id}>{guest.name}</div>
+        )}
     </div>
     <div>{data.contentfulEpisode.showNotes.internal.content}</div>
   </div>
