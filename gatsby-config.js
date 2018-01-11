20905fc1890c3c07e52f9d87a3b8aec854d4ac3b
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-01-11 14:19:35 -0700

    Add rss feed
    Closes #6

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,201 @@
 require('dotenv').config();
+const { DateTime } = require('luxon');
+var humanizeList = require('humanize-list');
+
 module.exports = {
   siteMetadata: {
     title: `Orbit FM`,
-    description: `Orbit FM is a place with podcasts.`
+    description: `Orbit FM is a place with podcasts.`,
+    siteUrl: `http://www.orbit.fm`,
+    owner: `Orbit FM`,
+    ownerEmail: `hello@orbit.fm`,
+    categories: ['Technology', 'Education'],
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        setup: ({
+          query: { site: { siteMetadata }, allContentfulPodcast },
+        }) => {
+          const podcast = allContentfulPodcast.edges[0].node;
+          return {
+            title: siteMetadata.title,
+            description: siteMetadata.description,
+            feed_url: `${siteMetadata.siteUrl}/${podcast.fields.slug}feed.rss`,
+            site_url: siteMetadata.siteUrl,
+            image_url: podcast.image ? podcast.image.file.url : ``,
+            managingEditor: `${siteMetadata.ownerEmail} (${
+              siteMetadata.owner
+            })`,
+            webMaster: `${siteMetadata.ownerEmail} (${siteMetadata.owner})`,
+            copyright: `${new Date().getFullYear()} ${siteMetadata.owner}`,
+            language: 'en',
+            categories: siteMetadata.categories,
+            pubDate: DateTime.fromISO(new Date()).toHTTP(),
+            ttl: '60',
+            custom_namespaces: {
+              itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+            },
+            custom_elements: [
+              { 'itunes:subtitle': siteMetadata.description },
+              { 'itunes:author': siteMetadata.owner },
+              { 'itunes:explicit': 'clean' },
+              {
+                'itunes:summary': siteMetadata.description,
+              },
+              {
+                'itunes:owner': [
+                  { 'itunes:name': siteMetadata.owner },
+                  { 'itunes:email': siteMetadata.ownerEmail },
+                ],
+              },
+              {
+                'itunes:image': {
+                  _attr: {
+                    href: podcast.image ? podcast.image.file.url : ``,
+                  },
+                },
+              },
+              ...siteMetadata.categories.map(c => ({
+                'itunes:category': {
+                  _attr: {
+                    text: c,
+                  },
+                },
+              })),
+            ],
+          };
+        },
+        // Todo: generate array of feeds for each podcast
+        feeds: [
+          {
+            serialize: ({ query: { site, allContentfulPodcast } }) => {
+              const podcast = allContentfulPodcast.edges[0].node;
+              return podcast.episode.map(episode => ({
+                title: episode.name,
+                description:
+                  episode.shortDescription ||
+                  `Episode ${episode.episodeNumber}`,
+                url: episode.audioUrl,
+                guid: episode.id,
+                author: episode.hosts.map(h => h.name, { oxfordComma: true }),
+                enclosure: {
+                  url: episode.audioUrl,
+                  length: episode.audioLength,
+                  type: 'audio/mp3',
+                },
+                custom_elements: [
+                  {
+                    pubDate: DateTime.fromISO(episode.publicationDate).toHTTP(),
+                  },
+                  {
+                    'itunes:author': humanizeList(
+                      episode.hosts.map(h => h.name, { oxfordComma: true })
+                    ),
+                  },
+                  {
+                    'itunes:subtitle': episode.shortDescription,
+                  },
+                  {
+                    'itunes:summary': episode.shortDescription,
+                  },
+                  {
+                    'content:encoded': `<p>${episode.shortDescription}</p>${
+                      episode.fields ? episode.fields.showNotesFormatted : ``
+                    }`,
+                  },
+                  { 'itunes:explicit': 'clean' },
+                  {
+                    'itunes:image': {
+                      _attr: {
+                        href: episode.image
+                          ? episode.image.file.url
+                          : podcast.image ? podcast.image.file.url : ``,
+                      },
+                    },
+                  },
+                  { 'itunes:duration': episode.duration },
+                ],
+              }));
+            },
+            query: `
+            {
+              site {
+                siteMetadata {
+                  title
+                  description
+                  siteUrl
+                  owner
+                  ownerEmail
+                  categories
+                }
+              }
+              allContentfulPodcast {
+                edges {
+                  node {
+                    id
+                    image {
+                      file {
+                        url
+                      }
+                    }
+                    fields {
+                      slug
+                    }
+                    episode {
+                      id
+                      episodeNumber
+                      audioUrl
+                      name
+                      shortDescription
+                      publicationDate
+                      audioLength
+                      duration
+                      hosts {
+                        name
+                      }
+                      fields {
+                        showNotesFormatted
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: '/weboftomorrow/feed.rss',
+          },
+        ],
+      },
+    },
+
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
+        pathToConfigModule: `src/utils/typography.js`,
+      },
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-    }
-  ]
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 };
