b3dc3d0af708e572cb802fd7a0f8be3e46dadb34
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-01-12 10:53:09 -0700

    Add local gatsby plugin

---
+++ plugins/gatsby-plugin-podcast-feed/readme.md
@@ -0,0 +1,25 @@
+# gatsby-plugin-podcast-feed
+
+Create a podcast RSS feed (or multiple feeds) for your Gatsby site.
+
+## Install
+
+```sh
+npm install --save gatsby-plugin-podcast-feed
+```
+
+## How to Use
+
+```
+// In your gatsby-config.js
+siteMetadata {
+  title: `GatsbyJS`,
+  description: `A fantastic new static site generator.`,
+  siteUrl: `https://www.gatsbyjs.org`
+},
+plugins: [
+  {
+    resolve: `gatsby-plugin-podcast-feed`
+  }
+]
+```
---
+++ plugins/gatsby-plugin-podcast-feed/src/gatsby-node.js
@@ -0,0 +1,63 @@
+import path from "path"
+import RSS from "rss"
+import merge from "lodash.merge"
+import { defaultOptions, runQuery, writeFile } from "./internals"
+
+const publicPath = `./public`
+
+// A default function to transform query data into feed entries.
+const serialize = ({ query: { site, allMarkdownRemark } }) =>
+  allMarkdownRemark.edges.map(edge => {
+    return {
+      ...edge.node.frontmatter,
+      description: edge.node.excerpt,
+      url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+      guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+      custom_elements: [{ "content:encoded": edge.node.html }],
+    }
+  })
+
+exports.onPostBuild = async ({ graphql }, pluginOptions) => {
+  delete pluginOptions.plugins
+
+  /*
+   * Run the site settings query to gather context, then
+   * then run the corresponding feed for each query.
+   */
+  const options = {
+    ...defaultOptions,
+    ...pluginOptions,
+  }
+
+  if (`query` in options) {
+    options.query = await runQuery(graphql, options.query)
+  }
+
+  const newFeeds = typeof options.feeds === 'function' ? options.feeds({query: options.query}) : options.feeds;
+
+  for (let f of newFeeds) {
+    if (f.query) {
+      f.query = await runQuery(graphql, f.query)
+
+      if (options.query) {
+        f.query = merge(options.query, f.query)
+        delete options.query
+      }
+    }
+
+    const { setup, ...locals } = {
+      ...options,
+      ...f,
+    }
+
+    const feed = new RSS(setup(locals))
+    const serializer =
+      f.serialize && typeof f.serialize === `function` ? f.serialize : serialize
+    const items = serializer(locals)
+
+    items.forEach(i => feed.item(i))
+    await writeFile(path.join(publicPath, f.output), feed.xml())
+  }
+
+  return Promise.resolve()
+}
