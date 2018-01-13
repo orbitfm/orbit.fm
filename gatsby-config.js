1e20d101170793af07d9bf965177f91444fda96f
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-01-12 19:42:16 -0700

    Sort episode in feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,53 +4,63 @@ var humanizeList = require('humanize-list');
 
 const serialize = podcast =>
   podcast.episode
-    ? podcast.episode.map(episode => ({
-        title: episode.name,
-        description:
-          episode.shortDescription || `Episode ${episode.episodeNumber}`,
-        url: episode.audioUrl,
-        guid: episode.id,
-        author: episode.hosts.map(h => h.name, {
-          oxfordComma: true,
-        }),
-        enclosure: {
+    ? podcast.episode
+        .sort((a, b) => {
+          if (a.publicationDate > b.publicationDate) {
+            return -1;
+          }
+          if (b.publicationDate > a.publicationDate) {
+            return 1;
+          }
+          return 0;
+        })
+        .map(episode => ({
+          title: episode.name,
+          description:
+            episode.shortDescription || `Episode ${episode.episodeNumber}`,
           url: episode.audioUrl,
-          length: episode.audioLength,
-          type: 'audio/mp3',
-        },
-        custom_elements: [
-          {
-            pubDate: DateTime.fromISO(episode.publicationDate).toHTTP(),
+          guid: episode.id,
+          author: episode.hosts.map(h => h.name, {
+            oxfordComma: true,
+          }),
+          enclosure: {
+            url: episode.audioUrl,
+            length: episode.audioLength,
+            type: 'audio/mp3',
           },
-          {
-            'itunes:author': humanizeList(
-              episode.hosts.map(h => h.name, { oxfordComma: true })
-            ),
-          },
-          {
-            'itunes:subtitle': episode.shortDescription,
-          },
-          {
-            'itunes:summary': episode.shortDescription,
-          },
-          {
-            'content:encoded': `<p>${episode.shortDescription}</p>${
-              episode.fields ? episode.fields.showNotesFormatted : ``
-            }`,
-          },
-          { 'itunes:explicit': 'clean' },
-          {
-            'itunes:image': {
-              _attr: {
-                href: episode.image
-                  ? episode.image.file.url
-                  : podcast.image ? podcast.image.file.url : ``,
+          custom_elements: [
+            {
+              pubDate: DateTime.fromISO(episode.publicationDate).toHTTP(),
+            },
+            {
+              'itunes:author': humanizeList(
+                episode.hosts.map(h => h.name, { oxfordComma: true })
+              ),
+            },
+            {
+              'itunes:subtitle': episode.shortDescription,
+            },
+            {
+              'itunes:summary': episode.shortDescription,
+            },
+            {
+              'content:encoded': `<p>${episode.shortDescription}</p>${
+                episode.fields ? episode.fields.showNotesFormatted : ``
+              }`,
+            },
+            { 'itunes:explicit': 'clean' },
+            {
+              'itunes:image': {
+                _attr: {
+                  href: episode.image
+                    ? episode.image.file.url
+                    : podcast.image ? podcast.image.file.url : ``,
+                },
               },
             },
-          },
-          { 'itunes:duration': episode.duration },
-        ],
-      }))
+            { 'itunes:duration': episode.duration },
+          ],
+        }))
     : [];
 
 module.exports = {
