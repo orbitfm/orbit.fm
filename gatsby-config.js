a725ea71d453f77d0878e0b1b1bc4e5caf2eed2b
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-06-20 07:57:36 -0600

    Add netlify redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const { DateTime } = require("luxon");
-var humanizeList = require("humanize-list");
+require('dotenv').config();
+const {DateTime} = require('luxon');
+var humanizeList = require('humanize-list');
 
-const serialize = ({ podcast, siteMetadata }) =>
+const serialize = ({podcast, siteMetadata}) =>
   podcast.episode
     ? podcast.episode
         .sort((a, b) => {
@@ -21,47 +21,47 @@ const serialize = ({ podcast, siteMetadata }) =>
           url: `${siteMetadata.siteUrl}/${episode.fields.path}`,
           guid: episode.id,
           author: episode.hosts.map(h => h.name, {
-            oxfordComma: true
+            oxfordComma: true,
           }),
           enclosure: {
             url: `https://www.podtrac.com/pts/redirect.mp3/${episode.audioUrl}`,
             length: episode.audioLength,
-            type: "audio/mp3"
+            type: 'audio/mp3',
           },
           custom_elements: [
             {
-              pubDate: DateTime.fromISO(episode.publicationDate).toHTTP()
+              pubDate: DateTime.fromISO(episode.publicationDate).toHTTP(),
             },
             {
-              "itunes:author": humanizeList(
-                episode.hosts.map(h => h.name, { oxfordComma: true })
-              )
+              'itunes:author': humanizeList(
+                episode.hosts.map(h => h.name, {oxfordComma: true})
+              ),
             },
             {
-              "itunes:subtitle": episode.shortDescription
+              'itunes:subtitle': episode.shortDescription,
             },
             {
-              "itunes:summary": episode.shortDescription
+              'itunes:summary': episode.shortDescription,
             },
             {
-              "content:encoded": `<p>${episode.shortDescription}</p>${
+              'content:encoded': `<p>${episode.shortDescription}</p>${
                 episode.fields ? episode.fields.showNotesFormatted : ``
-              }`
+              }`,
             },
-            { "itunes:explicit": "clean" },
+            {'itunes:explicit': 'clean'},
             {
-              "itunes:image": {
+              'itunes:image': {
                 _attr: {
                   href: episode.image
                     ? `https:${episode.image.file.url}`
                     : episode.podcast.image
                       ? `https:${episode.podcast.image.file.url}`
-                      : ``
-                }
-              }
+                      : ``,
+                },
+              },
             },
-            { "itunes:duration": episode.duration }
-          ]
+            {'itunes:duration': episode.duration},
+          ],
         }))
     : [];
 
@@ -73,7 +73,7 @@ module.exports = {
     siteUrl: `https://www.orbit.fm`,
     owner: `Orbit FM`,
     ownerEmail: `hello@orbit.fm`,
-    categories: ["Technology", "Education"]
+    categories: ['Technology', 'Education'],
   },
   plugins: [
     {
@@ -142,7 +142,12 @@ module.exports = {
         }
       `,
         setup: (
-          { query: { site: { siteMetadata }, allContentfulPodcast } },
+          {
+            query: {
+              site: {siteMetadata},
+              allContentfulPodcast,
+            },
+          },
           i
         ) => {
           // If allContentfulPodcast.edges[i] then this is a normal feed
@@ -152,10 +157,10 @@ module.exports = {
             : {
                 name: `${siteMetadata.title} master feed`,
                 fields: {
-                  slug: "master"
+                  slug: 'master',
                 },
-                description: { description: siteMetadata.description },
-                image: { file: { url: siteMetadata.coverArt } }
+                description: {description: siteMetadata.description},
+                image: {file: {url: siteMetadata.coverArt}},
               };
           return {
             title: podcast.name,
@@ -171,71 +176,79 @@ module.exports = {
             })`,
             webMaster: `${siteMetadata.ownerEmail} (${siteMetadata.owner})`,
             copyright: `${new Date().getFullYear()} ${siteMetadata.owner}`,
-            language: "en",
+            language: 'en',
             categories: siteMetadata.categories,
             pubDate: DateTime.fromISO(new Date()).toHTTP(),
-            ttl: "60",
+            ttl: '60',
             custom_namespaces: {
-              itunes: "http://www.itunes.com/dtds/podcast-1.0.dtd"
+              itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd',
             },
             custom_elements: [
-              { "itunes:subtitle": siteMetadata.description },
-              { "itunes:author": siteMetadata.owner },
-              { "itunes:explicit": "clean" },
+              {'itunes:subtitle': siteMetadata.description},
+              {'itunes:author': siteMetadata.owner},
+              {'itunes:explicit': 'clean'},
               {
-                "itunes:summary": siteMetadata.description
+                'itunes:summary': siteMetadata.description,
               },
               {
-                "itunes:owner": [
-                  { "itunes:name": siteMetadata.owner },
-                  { "itunes:email": siteMetadata.ownerEmail }
-                ]
+                'itunes:owner': [
+                  {'itunes:name': siteMetadata.owner},
+                  {'itunes:email': siteMetadata.ownerEmail},
+                ],
               },
               {
-                "itunes:image": {
+                'itunes:image': {
                   _attr: {
-                    href: podcast.image ? `https:${podcast.image.file.url}` : ``
-                  }
-                }
+                    href: podcast.image
+                      ? `https:${podcast.image.file.url}`
+                      : ``,
+                  },
+                },
               },
               ...siteMetadata.categories.map(c => ({
-                "itunes:category": {
+                'itunes:category': {
                   _attr: {
-                    text: c
-                  }
-                }
-              }))
-            ]
+                    text: c,
+                  },
+                },
+              })),
+            ],
           };
         },
         feeds: ({
-          query: { site: { siteMetadata }, allContentfulPodcast }
+          query: {
+            site: {siteMetadata},
+            allContentfulPodcast,
+          },
         }) => [
-          ...allContentfulPodcast.edges.map(({ node }, i) => ({
-            serialize: ({ query: { site, allContentfulPodcast } }) =>
+          ...allContentfulPodcast.edges.map(({node}, i) => ({
+            serialize: ({query: {site, allContentfulPodcast}}) =>
               serialize({
                 podcast: allContentfulPodcast.edges[i].node,
-                siteMetadata
+                siteMetadata,
               }),
-            output: `${node.fields.slug}/feed.rss`
+            output: `${node.fields.slug}/feed.rss`,
           })),
           {
             serialize: ({
-              query: { site: { siteMetadata }, allContentfulPodcast }
+              query: {
+                site: {siteMetadata},
+                allContentfulPodcast,
+              },
             }) =>
               serialize({
                 podcast: {
                   episode: allContentfulPodcast.edges.reduce(
-                    (a, { node }) => [...a, ...(node.episode || [])],
+                    (a, {node}) => [...a, ...(node.episode || [])],
                     []
-                  )
+                  ),
                 },
-                siteMetadata
+                siteMetadata,
               }),
-            output: `master.rss`
-          }
-        ]
-      }
+            output: `master.rss`,
+          },
+        ],
+      },
     },
 
     `gatsby-plugin-react-helmet`,
@@ -243,30 +256,31 @@ module.exports = {
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
+        pathToConfigModule: `src/utils/typography.js`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/transcripts`,
-        name: "markdown-pages",
+        name: 'markdown-pages',
       },
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-        anonymize: true
-      }
+        anonymize: true,
+      },
     },
     `gatsby-transformer-json`,
-  ]
+    `gatsby-plugin-netlify`,
+  ],
 };
