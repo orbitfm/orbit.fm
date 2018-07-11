c6779ba087877ba8c2bba3470c18f276b86eb4a9
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-07-11 12:25:09 -0600

    Fix gatsby-node when a person's description doesn't exist

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,10 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 const path = require(`path`);
-const Remarkable = require("remarkable");
+const Remarkable = require('remarkable');
 const markdown = new Remarkable({ html: true });
 const slug = require(`slug`);
-const urlify = a => a.replace(/\s/g, "").toLowerCase();
+const urlify = a => a.replace(/\s/g, '').toLowerCase();
 
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators;
@@ -16,14 +16,14 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
     PODCAST: `ContentfulPodcast`,
     EPISODE: `ContentfulEpisode`,
     PERSON: `ContentfulPerson`,
-    EPISODE__SHOW_NOTES: "contentfulEpisodeShowNotesTextNode"
+    EPISODE__SHOW_NOTES: 'contentfulEpisodeShowNotesTextNode',
   };
 
   if (node.internal.type === types.PODCAST) {
     createNodeField({
       node,
       name: `slug`,
-      value: urlify(node.name)
+      value: urlify(node.name),
     });
   }
   if (node.internal.type === types.EPISODE) {
@@ -36,7 +36,7 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
           : node.episodeNumber !== undefined
             ? node.episodeNumber
             : slug(node.name, { lower: true })
-      }`
+      }`,
     });
 
     if (node.showNotes___NODE) {
@@ -44,8 +44,8 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
       if (showNotes && showNotes.internal) {
         createNodeField({
           node,
-          name: "showNotesFormatted",
-          value: markdown.render(showNotes.internal.content)
+          name: 'showNotesFormatted',
+          value: markdown.render(showNotes.internal.content),
         });
       }
     }
@@ -55,15 +55,16 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
     createNodeField({
       node,
       name: `slug`,
-      value: slug(node.name, { lower: true })
+      value: slug(node.name, { lower: true }),
     });
-    const description = node.description___NODE
-      ? getNode(node.description___NODE).internal.content
-      : "";
+    const description =
+      node.description___NODE && getNode(node.description___NODE).internal
+        ? getNode(node.description___NODE).internal.content
+        : '';
     createNodeField({
       node,
-      name: "descriptionFormatted",
-      value: markdown.render(description)
+      name: 'descriptionFormatted',
+      value: markdown.render(description),
     });
   }
 };
@@ -115,8 +116,8 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
           component: path.resolve(`./src/templates/podcast.js`),
           context: {
             // Data passed to context is available in page queries as GraphQL variables.
-            id: node.id
-          }
+            id: node.id,
+          },
         });
       });
       result.data.allContentfulEpisode.edges.forEach(({ node }) => {
@@ -128,7 +129,7 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
             id: node.id,
             podcastName: node.podcast.name,
             episodeNumber: node.episodeNumber,
-          }
+          },
         });
       });
       result.data.allContentfulPerson.edges.forEach(({ node }) => {
@@ -137,8 +138,8 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
           component: path.resolve(`./src/templates/person.js`),
           context: {
             // Data passed to context is available in page queries as GraphQL variables.
-            id: node.id
-          }
+            id: node.id,
+          },
         });
       });
       resolve();
