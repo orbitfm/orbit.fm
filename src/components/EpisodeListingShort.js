13f92dad7f9336c972059715611d1dad50951625
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-07-13 08:45:06 -0600

    Add shows to person page (#99)
    - List shows in which the person is a host
    - List episodes that appear in (either as a guest or host)
    - Move links below description

---
+++ src/components/EpisodeListingShort.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import styled from 'react-emotion';
+import { DateTime } from 'luxon';
+
+const Header = styled.header`
+  font-size: 1.5em;
+  padding-bottom: 16px;
+`;
+
+const EpisodeContainer = styled.ul`
+  margin-left: 0;
+`;
+
+const EpisodeItem = styled.li`
+  display: flex;
+  align-items: center;
+  list-style: none;
+`;
+
+const Title = styled.span`
+  margin-right: 20px;
+`;
+
+const EpisodeDate = styled.span`
+  font-size: 0.7em;
+`;
+
+const EpisodePodcastImage = styled.img`
+  height: 20px;
+  width: auto;
+  margin: 0 20px 0 0;
+`;
+
+const Episode = ({ episode }) => (
+  <EpisodeContainer>
+    <EpisodeItem>
+      <Link to={episode.fields.path}>
+        <EpisodePodcastImage
+          src={episode.podcast.image.file.url}
+          alt={`${episode.podcast.name} logo`}
+        />
+      </Link>
+      <Link to={episode.fields.path}>
+        <Title>
+          {episode.podcast.name} {episode.name}
+        </Title>
+      </Link>
+      <EpisodeDate>
+        {DateTime.fromISO(episode.publicationDate).toLocaleString()}
+      </EpisodeDate>
+    </EpisodeItem>
+  </EpisodeContainer>
+);
+
+const EpisodeListingShort = ({ episodes }) => {
+  return (
+    <section>
+      <Header>Appearances</Header>
+      {episodes
+        .sort(
+          (a, b) => new Date(b.publicationDate) - new Date(a.publicationDate)
+        )
+        .map(episode => <Episode episode={episode} />)}
+    </section>
+  );
+};
+
+export default EpisodeListingShort;
--- src/templates/person.js
+++ src/templates/person.js
@@ -1,44 +1,77 @@
-import React from "react";
-import Link from "gatsby-link";
-import styled from "react-emotion";
-import Page from "../components/Page";
+import React from 'react';
+import styled from 'react-emotion';
+import Page from '../components/Page';
+import PodcastListing from '../components/PodcastListing';
+import EpisodeListingShort from '../components/EpisodeListingShort';
 
 const CircleImage = styled.img`
-  float: left;
   border-radius: 50%;
-  shape-outside: circle();
   margin-right: 20px;
+`;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Description = styled.div`
+  flex: 0;
+`;
+
+const PersonDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const Links = styled.div`
+  display: flex;
+
+  a {
+    margin-right: 12px;
+  }
 `;
 
 export default ({ data }) => {
   const person = data.contentfulPerson;
   return (
     <Page title={person.name} headTitle={person.name}>
-      {person.image && (
-        <CircleImage src={person.image.file.url} width="100px" height="100px" />
-      )}
-      <div
-        dangerouslySetInnerHTML={{
-          __html: person.fields.descriptionFormatted
-        }}
-      />
-      {person.links &&
-        person.links.map(link => (
-          <a
-            href={link.url}
-            target="
+      <Container>
+        {person.image && (
+          <CircleImage
+            src={person.image.file.url}
+            width="100px"
+            height="100px"
+          />
+        )}
+        <PersonDetails>
+          <Description
+            dangerouslySetInnerHTML={{
+              __html: person.fields.descriptionFormatted,
+            }}
+          />
+          <Links>
+            {person.links &&
+              person.links.map(link => (
+                <a
+                  href={link.url}
+                  target="
       _blank"
-            rel="noopener"
-            key={link.id}
-          >
-            <img
-              title={link.linkType.name}
-              src={link.linkType.image.file.url}
-              width="20px"
-              height="20px"
-            />
-          </a>
-        ))}
+                  rel="noopener"
+                  key={link.id}
+                >
+                  <img
+                    title={link.linkType.name}
+                    src={link.linkType.image.file.url}
+                    width="20px"
+                    height="20px"
+                  />
+                </a>
+              ))}
+          </Links>
+        </PersonDetails>
+      </Container>
+      {person.podcast && <PodcastListing podcasts={person.podcast} />}
+      {person.episode && <EpisodeListingShort episodes={person.episode} />}
     </Page>
   );
 };
@@ -67,6 +100,40 @@ export const query = graphql`
               url
               fileName
               contentType
+            }
+          }
+        }
+      }
+      podcast {
+        id
+        name
+        description {
+          description
+        }
+        image {
+          file {
+            url
+          }
+        }
+        fields {
+          slug
+        }
+        episode {
+          id
+        }
+      }
+      episode {
+        id
+        name
+        publicationDate
+        fields {
+          path
+        }
+        podcast {
+          name
+          image {
+            file {
+              url
             }
           }
         }
