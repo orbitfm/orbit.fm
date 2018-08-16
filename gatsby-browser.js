d4904fa6f7efefbf4d0740cd3b02b1f9b40d335d
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-08-16 12:50:09 -0600

    Use emotion's injectGlobal

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { injectGlobal } from 'emotion';
 
 import createStore from './src/state/createStore';
-import './src/components/layout.css';
+
+injectGlobal`
+body {
+  margin: 0;
+  color: white;
+  word-wrap: break-word;
+  box-sizing: border-box;
+  background: #333;
+}
+a {
+  color: white;
+}
+a:active,
+a:hover {
+  outline-width: 0;
+  color: white;
+  text-decoration: underline;
+}
+html {
+  box-sizing: border-box;
+  overflow-y: scroll;
+}
+* {
+  box-sizing: inherit;
+}
+*:before {
+  box-sizing: inherit;
+}
+*:after {
+  box-sizing: inherit;
+}
+`;
 
 const store = createStore();
 
