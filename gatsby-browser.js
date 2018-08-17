0c922ed76e95b196957c5d408232450e47e67626
Jason Lengstorf <jason@lengstorf.com>
2018-08-17 15:33:49 -0700

    fix: import styles on front-end and during SSR to avoid FOUC

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,40 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { injectGlobal } from 'emotion';
 
 import createStore from './src/state/createStore';
 
-injectGlobal`
-body {
-  margin: 0;
-  color: white;
-  word-wrap: break-word;
-  box-sizing: border-box;
-  background: #333;
-}
-a {
-  color: white;
-}
-a:active,
-a:hover {
-  outline-width: 0;
-  color: white;
-  text-decoration: underline;
-}
-html {
-  box-sizing: border-box;
-  overflow-y: scroll;
-}
-* {
-  box-sizing: inherit;
-}
-*:before {
-  box-sizing: inherit;
-}
-*:after {
-  box-sizing: inherit;
-}
-`;
+// Import global styles here
+import './src/utils/global-styles';
 
 const store = createStore();
 
---
+++ src/utils/global-styles.js
@@ -0,0 +1,33 @@
+import { injectGlobal } from 'emotion';
+
+injectGlobal`
+  body {
+    margin: 0;
+    color: white;
+    word-wrap: break-word;
+    box-sizing: border-box;
+    background: #333;
+  }
+  a {
+    color: white;
+  }
+  a:active,
+  a:hover {
+    outline-width: 0;
+    color: white;
+    text-decoration: underline;
+  }
+  html {
+    box-sizing: border-box;
+    overflow-y: scroll;
+  }
+  * {
+    box-sizing: inherit;
+  }
+  *:before {
+    box-sizing: inherit;
+  }
+  *:after {
+    box-sizing: inherit;
+  }
+`;
