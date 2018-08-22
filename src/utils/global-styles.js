28f93abd039c1f38b9c19ecfa46168c24ae96be5
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-08-22 17:31:52 -0600

    Override typography theme

--- src/utils/global-styles.js
+++ src/utils/global-styles.js
@@ -1,24 +1,14 @@
 import { injectGlobal } from 'emotion';
+
 injectGlobal`
   body {
     margin: 0;
     color: white;
-    word-wrap: break-word;
     box-sizing: border-box;
     background: #333;
   }
-  a {
-    color: white;
-  }
-  a:active,
-  a:hover {
-    outline-width: 0;
-    color: white;
-    text-decoration: underline;
-  }
   html {
     box-sizing: border-box;
-    overflow-y: scroll;
   }
   * {
     box-sizing: inherit;
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,17 @@
 import Typography from 'typography';
 import altonTheme from 'typography-theme-alton';
 
+altonTheme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
+  a: {
+    color: 'white',
+    textDecoration: 'inherit',
+  },
+  'a:hover,a:active': {
+    color: 'white',
+    textDecoration: 'underline',
+  },
+});
+
 const typography = new Typography(altonTheme);
 const { rhythm, scale } = typography;
 export { rhythm, scale, typography as default };
